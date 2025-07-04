a94033c0333dbbbf80820c4386b5dd07f488cf16
Ephraim Debel <ephraimdebel@gmail.com>
2025-07-04 08:12:30 +0300

    cors update

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,16 @@ const app = express();
 
 connectDB();
 
+// const corsOptions = {
+//   origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5173', 'http://localhost:3000'],
+//   credentials: true,
+//   optionsSuccessStatus: 200
+// };
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5173', 'http://localhost:3000'],
+  origin: true, // Allows all origins
   credentials: true,
   optionsSuccessStatus: 200
 };
-
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
